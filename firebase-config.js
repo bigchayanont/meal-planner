1f899b3606f255e887f54cf5142e50accf605cc8
Chayanont Piyawatcharavijit <chayanont.piyawatcharavijit@lynxx.com>
2026-06-11 00:00:01 +1000

    Update Firebase database URL

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyDFO8X4PZZUa585Rp4Fx89u8Bl3mvIQlQ8",
   authDomain: "meal-planner-dbf11.firebaseapp.com",
-  databaseURL: "https://meal-planner-dbf11-default-rtdb.firebaseio.com",
+  databaseURL: "https://meal-planner-dbf11-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "meal-planner-dbf11",
   storageBucket: "meal-planner-dbf11.firebasestorage.app",
   messagingSenderId: "374482797553",
